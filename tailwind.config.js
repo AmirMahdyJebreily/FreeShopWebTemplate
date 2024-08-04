bc12300eedbe36498bfc3ce459537a0dc123a30e
Amir JCS <amirmahdyjdx@gmail.com>
2024-08-04 22:12:14 +0330

    build: init tools and fonts

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      'nav-title': ['"Dancing Script"', 'cursive']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
